37693c7f2ebf746ff35e04f1a27eab0899eff835
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-05-20 13:10:10 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
 end
