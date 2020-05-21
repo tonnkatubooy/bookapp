1c26c4fe870dbbfcbe87176bb9326caa3fa984eb
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-05-21 17:04:03 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  get 'messages/create'
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
   resources :users, only: [:edit, :update, :show]
-  resources :groups, only: [:new, :create,:edit,:update,:show]
+  resources :groups, only: [:new, :create,:edit,:update,:show] do
+    resources :messages, only: [:index,:create]
+  end
 end
