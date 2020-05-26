0e73f99672f03cc74081c0efb8f20902188b00cc
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-05-26 17:20:16 +0900

    view edit

--- app/assets/javascripts/toppage.js
+++ app/assets/javascripts/toppage.js
@@ -1,6 +1,12 @@
-$(document).ready(function(){
-  $('fa-users').on('click',function(){
-    console.log('test');
-    $(this).next().toggleClass('memberLists');
-  });
-});
+$(document).on('mouseover','#icon',function(){
+  $(this).next().toggleClass('memberLists');
+})
+
+$(document).on('mouseover','upper',function(){
+  console.log('test')
+  $(this).next().toggleClass('hidden');
+})
+// $('fa-users').on('click',function(){
+//   console.log('test');
+//   $(this).next().toggleClass('memberLists');
+// });
