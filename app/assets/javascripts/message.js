32f440df658ef41e8e8b94d404372c9c220bc212
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-05-29 16:56:32 +0900

    add js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,3 +5,24 @@ $(document).on('click','#start-btn',function(){
     window.alert('本を準備してね！')
   }
 })
+
+function countdown(due){
+  const now = new Date();
+
+  const rest = due.getTime() - now.getTime();
+  const sec = Math.floor(rest / 1000) % 60;
+  const min = Math.floor(rest / 1000 / 60) % 60;
+  const hours = Math.floor(rest / 1000 / 60 / 60) % 24;
+  const count = [hours,min,sec];
+
+  return count;
+}
+
+let goal = new Date();
+goal.setMinutes(59);
+goal.setSeconds(59);
+
+console.log(countdown(goal));
+const counter = countdown(goal);
+const time = `${counter[0]}分${counter[1]}秒`
+document.getElementById('timer').textContent = time;
