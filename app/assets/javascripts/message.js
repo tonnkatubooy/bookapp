7a71b6ef4aa7f97aa3436d1f42b8097175f7430a
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-06-03 09:44:57 +0900

    edit js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -6,23 +6,53 @@ $(document).on('click','#start-btn',function(){
   }
 })
 
-function countdown(due){
-  const now = new Date();
 
-  const rest = due.getTime() - now.getTime();
-  const sec = Math.floor(rest / 1000) % 60;
-  const min = Math.floor(rest / 1000 / 60) % 60;
-  const hours = Math.floor(rest / 1000 / 60 / 60) % 24;
-  const count = [hours,min,sec];
+var interval_id;
+var start_click = false;
+var time = 10;
+var min = 0;
+var sec = 0;
 
-  return count;
+function count_start() {
+    if (start_click === false) {
+        interval_id = setInterval(count_down, 1000);
+        start_click = true;
+    }
 }
 
-let goal = new Date();
-goal.setMinutes(59);
-goal.setSeconds(59);
+function count_down() {
+    var display = document.getElementById('display');
+    if (time === 0) {
+        window.alert(display.innerHTML = '読書を終了してください');
+        count_stop();
+    } else {
+        time--;
+        min = Math.floor(time / 60);
+        sec = Math.floor(time % 60); // senをsecに修正。
 
-console.log(countdown(goal));
-const counter = countdown(goal);
-const time = `${counter[0]}分${counter[1]}秒`
-document.getElementById('timer').textContent = time;
+        display.innerHTML = '0' + min + ':' + ( sec < 10 ? '0' : '') + sec;// 修正、追記。
+    }
+}
+
+function count_stop() {
+    clearInterval(interval_id);
+    start_click = false;
+}
+
+function count_reset() {
+    time = 10;
+    min = 0;
+    sec = 0; // senをsecに修正。
+    var reset = document.getElementById('display');
+    reset.innerHTML = '00:10';
+
+}
+
+window.onload = function () {
+    var start = document.getElementById('start');
+    start.addEventListener('click', count_start, false);
+    var stop = document.getElementById('stop');
+    stop.addEventListener('click', count_stop, false);
+    var reset = document.getElementById('reset');
+    reset.addEventListener('click', count_reset, false);
+}
